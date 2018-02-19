4bc49c21670537c52f450e4814df121cbfa4c25f
maheratashfaraz <maheratashfaraz2000@yahoo.com>
2018-02-19 15:47:30 +0000

    Added photo taking logic

--- app/src/main/java/digital/and/andexpenses/addexpense/AndExpenseActivity.java
+++ app/src/main/java/digital/and/andexpenses/addexpense/AndExpenseActivity.java
@@ -1,10 +1,12 @@
 package digital.and.andexpenses.addexpense;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import dagger.android.AndroidInjection;
 import digital.and.andexpenses.R;
@@ -30,6 +32,14 @@ public class AndExpenseActivity extends AppCompatActivity{
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
         }
     }
 
