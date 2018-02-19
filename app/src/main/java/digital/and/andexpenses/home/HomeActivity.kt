e84bbd3649bfe64021daed8ab3a070531efdf938
Chris Mason <chrismasonfm@hotmail.co.uk>
2018-02-19 10:30:42 +0000

    Initial commit

---
+++ app/src/main/java/digital/and/andexpenses/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package digital.and.andexpenses.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import digital.and.andexpenses.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
