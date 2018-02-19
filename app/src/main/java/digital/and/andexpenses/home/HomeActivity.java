61ec3fe0b8d0c62603c4285669d631a487e45b43
Chris Mason <chrismasonfm@hotmail.co.uk>
2018-02-19 11:24:06 +0000

    Changed activity to Java

---
+++ app/src/main/java/digital/and/andexpenses/home/HomeActivity.java
@@ -0,0 +1,25 @@
+package digital.and.andexpenses.home;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import dagger.android.AndroidInjection;
+import digital.and.andexpenses.R;
+
+/**
+ * Created by cmason on 19/02/2018.
+ */
+
+public class HomeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+}
